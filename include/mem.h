d9afd42459cb3509f004c05bc71b9bf823f17a02
Nico Knünz <nico.knuenz@aon.at>
2024-10-01 23:20:05 +0200

    fix: correct heap-size with splitting into bytes

--- include/mem.h
+++ include/mem.h
@@ -8,12 +8,14 @@
 #define HEAP_SIZE 1024
 #endif
 
+#define HIGH_BYTE(x) ((uint8_t)((x) >> 8))
+#define LOW_BYTE(x) ((uint8_t)((x) & 0xFF))
+
 #define FREE 0
 #define OCCUPIED 1
 
-void* mem_malloc(uint16_t size);
+void *mem_malloc(uint16_t size);
 
-void mem_free(void* addr);
+void mem_free(void *addr);
 
 #endif
-
--- mem.c
+++ mem.c
@@ -1,24 +1,27 @@
 #include "include/mem.h"
 
-typedef struct {
+typedef struct
+{
     uint8_t flag;
     uint8_t rfu;
     uint16_t len;
 } mem_header_t;
 
+static uint8_t gHeap[HEAP_SIZE] = {FREE, 0x0, HIGH_BYTE(HEAP_SIZE), LOW_BYTE(HEAP_SIZE)};
 
-static uint8_t gHeap[HEAP_SIZE] = {FREE, 0x0, 0xFC, 0x03};
+// header
+void *mem_malloc(uint16_t size)
+{
+    // search for free memory -> start at the beginning
+    uint8_t *ptr = gHeap;
 
-// header 
-void* mem_malloc(uint16_t size) {
-    //search for free memory -> start at the beginning
-    uint8_t* ptr = gHeap;
-    
     // loop over until we find free space or reach the end of the heap
-    while (ptr < gHeap + HEAP_SIZE) {
-        mem_header_t* mem_header = (mem_header_t*)ptr;
-        
-        if (!mem_header->flag && mem_header->len >= size) {
+    while (ptr < gHeap + HEAP_SIZE)
+    {
+        mem_header_t *mem_header = (mem_header_t *)ptr;
+
+        if (!mem_header->flag && mem_header->len >= size)
+        {
             // save remaining length before overwritting the header
             uint16_t remaining_length = mem_header->len - size;
 
@@ -28,15 +31,16 @@ void* mem_malloc(uint16_t size) {
 
             // check if there is enough space to create a new free block
             // if not we either should have another block that should have a free block defined or we reached end of heap
-            if (remaining_length > sizeof(mem_header_t)) {
-                mem_header_t* new_header = (mem_header_t*)(ptr + sizeof(mem_header_t) + size);
+            if (remaining_length > sizeof(mem_header_t))
+            {
+                mem_header_t *new_header = (mem_header_t *)(ptr + sizeof(mem_header_t) + size);
                 new_header->flag = FREE;
                 new_header->rfu = 0;
                 new_header->len = remaining_length - sizeof(mem_header_t);
             }
 
             // void pointer to the data-segment of the allocated mem-block
-            return (void*)(ptr + sizeof(mem_header_t));
+            return (void *)(ptr + sizeof(mem_header_t));
         }
 
         // replace pointer to the next mem-block after the current one
@@ -47,9 +51,10 @@ void* mem_malloc(uint16_t size) {
     return NULL;
 }
 
-void mem_free(void* addr) {
+void mem_free(void *addr)
+{
     // get the header of the block
-    mem_header_t* mem_header = (mem_header_t*)((uint8_t*)addr - sizeof(mem_header_t));
+    mem_header_t *mem_header = (mem_header_t *)((uint8_t *)addr - sizeof(mem_header_t));
 
     // free the block
     mem_header->flag = FREE;
